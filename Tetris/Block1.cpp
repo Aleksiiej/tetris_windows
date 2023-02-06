f9028b07ddaad34af49fbe3e8ea8e5a9539e08fe
Aleksander Ramut <aleksander.ramut@gmail.com>
2023-02-07 00:02:23 +0100

    Redundant second std::for_each in blocks classes constructors removed

--- Tetris/Block1.cpp
+++ Tetris/Block1.cpp
@@ -3,8 +3,8 @@
 Block1::Block1(BlockBoard& blockBoardRef) noexcept
 	: BaseBlock(blockBoardRef)
 {
-	std::for_each(begin(blockArray_), end(blockArray_), [](auto& block) { block = sf::RectangleShape{ sf::Vector2f{ GRID, GRID } }; });
-	std::for_each(begin(blockArray_), end(blockArray_), [](auto& block) { block.setFillColor(sf::Color::Red); });
+	std::for_each(begin(blockArray_), end(blockArray_), [](auto& block) { block = sf::RectangleShape{ sf::Vector2f{ GRID, GRID } };
+	block.setFillColor(sf::Color::Red); });
 	blockArray_.at(0).setPosition((NUMBER_OF_COLUMNS / 2) * GRID, GRID);
 	blockArray_.at(1).setPosition((NUMBER_OF_COLUMNS / 2) * GRID + GRID, GRID);
 	blockArray_.at(2).setPosition((NUMBER_OF_COLUMNS / 2) * GRID, 2 * GRID);
